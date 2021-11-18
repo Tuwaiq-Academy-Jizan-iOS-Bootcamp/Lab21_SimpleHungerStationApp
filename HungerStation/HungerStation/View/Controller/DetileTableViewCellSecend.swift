56242898d02a3cdbd89fbff67189b04d6e6dc2d4
laila hakami <laila.hakami.jiz.s@tuwaiq.edu.sa>
2021-11-18 13:53:06 +0300

    LaylaHakami

---
+++ HungerStation/HungerStation/View/Controller/DetileTableViewCellSecend.swift
@@ -0,0 +1,22 @@
+//
+//  DetileTableViewCellSecend.swift
+//  HungerStation
+//
+//  Created by layla hakami on 12/04/1443 AH.
+//
+
+import Foundation
+import UIKit
+class DetileTableViewCellSecend: UITableViewCell {
+    
+    
+    @IBOutlet weak var mealImage: UIImageView!
+    
+    @IBOutlet weak var mealName: UILabel!
+    
+    @IBOutlet weak var mealprice: UILabel!
+    
+    
+    
+    
+}
---
+++ HungerStation/HungerStation/View/Controller/DetileTableViweCell .swift
@@ -0,0 +1,21 @@
+
+import Foundation
+import UIKit
+class DetileTableViweCell:UITableViewCell{
+   
+    @IBOutlet weak var restaurantLogo: UIImageView!
+     
+    @IBOutlet weak var restaurantImageView: UIImageView!
+      
+    @IBOutlet weak var restaurantName: UILabel!
+   
+    @IBOutlet weak var restaurantType: UILabel!
+    
+    @IBOutlet weak var restaurantDeliveryTime: UILabel!
+  
+    @IBOutlet weak var delivery: UILabel!
+    
+    @IBOutlet weak var starPoint: UILabel!
+    
+    
+}
