0907e4f050038b797354338be940a5c0c896aa56
yasir <donyasser4@gmail.com>
2021-11-21 09:19:33 +0300

    not finish yet

---
+++ Hanger/Hanger/Controller/MenuTableView.swift
@@ -0,0 +1,51 @@
+//
+//  MenuTableView.swift
+//  Hanger
+//
+//  Created by Yasir Hakami on 17/11/2021.
+//
+
+import Foundation
+import UIKit
+
+
+class MenuTableView: UIViewController {
+    
+    var menu = HungerStaion.restaurant[0].munuOfRestaurant
+    @IBOutlet weak var menuTableView: UITableView!
+    @IBOutlet weak var generalImage: UIImageView!
+    var imageOfresturant = UIImage(named: "")
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        menuTableView.delegate = self
+        menuTableView.dataSource = self
+        generalImage.image = imageOfresturant
+    }
+    
+}
+extension MenuTableView : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return menu.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = menuTableView.dequeueReusableCell(withIdentifier: "cellTwo", for: indexPath)
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = menu[indexPath.row].nameOfMeal
+        let imageSize = CGSize(width: 90, height: 90)
+        content.image = menu[indexPath.row].mealImages
+        content.imageProperties.maximumSize = imageSize
+        content.imageProperties.cornerRadius = .infinity
+        content.secondaryText = "\(menu[indexPath.row].PriceOfMeal) SRA "
+        
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+    
+}
