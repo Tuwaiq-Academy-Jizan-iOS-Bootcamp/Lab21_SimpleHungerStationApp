4c5c0405b4b71dfe5062a03f2db6adcf1a1192ea
Nasser Ali <naseeri1983@gmail.com>
2021-12-14 14:29:10 +0300

    Nasser

---
+++ Nasser_Lab21/Nasser_Lab21/FoodsCell.swift
@@ -0,0 +1,20 @@
+//
+//  FoodsCell.swift
+//  Nasser_Lab21
+//
+//  Created by Nasser Aseeri on 10/05/1443 AH.
+//
+
+import UIKit
+
+class FoodsCell: UITableViewCell {
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var restorandFoodImage: UIImageView!
+    @IBOutlet weak var restorantName: UILabel!
+    @IBOutlet weak var foodType: UILabel!
+    @IBOutlet weak var deliveryAndOtherThings: UILabel!
+    @IBOutlet weak var raiting: UILabel!
+    @IBOutlet weak var priceCondition: UILabel!
+    @IBOutlet weak var promoted: UILabel!
+}
+
