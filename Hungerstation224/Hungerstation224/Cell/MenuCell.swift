fbdd33ab8b63dc4ac979b24bc488452ab4944699
MUSAMASMALI <musa14077@yahoo.com>
2021-12-18 21:54:57 +0300

    "musa"

---
+++ Hungerstation224/Hungerstation224/Cell/MenuCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+class MenuCell: UITableViewCell {
+    @IBOutlet weak var foodImage: UIImageView!
+    @IBOutlet weak var kaloris: UILabel!
+    @IBOutlet weak var secondaryText: UILabel!
+    @IBOutlet weak var mainText: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var trashImage: UIImageView!
+}
+
